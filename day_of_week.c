649bf2644817095e3df38f1c08e8e695db58a14e
Iffat Humayra Refa <humayra.refa@gmail.com>
2026-06-23 20:36:43 +0600

    Tried some basic codes

---
+++ day_of_week.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+    int day_of_week = 0;
+    scanf("%d",&day_of_week);
+    switch(day_of_week){
+        case 1:
+        printf("Saturday");
+        break;
+        case 2:
+        printf("Sunday");
+        break;
+        case 3:
+        printf("Monday");
+        break;
+        case 4:
+        printf("Tuesday");
+        break;
+        case 5:
+        printf("Wednesday");
+        break;
+        case 6:
+        printf("Thursday");
+        break;
+        case 7:
+        printf("Friday");
+        break;
+        default:
+        printf("Invalid case");
+        break;
+
+
+    }
+}
